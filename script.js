1f3bc73988683737d356c30bc5984ab38fba94a4
manojkalyan27 <147662541+manojkalyan27@users.noreply.github.com>
2024-11-20 16:50:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,28 +1,37 @@
-//your JS code here. If required.
+// Your array
 let arr = [1, 2, 3, 4];
 
-let output = document.querySelector('#output');
+// Select the output div
+let output = document.querySelector("#output");
 
-function ChainedPromises(){
-
-	return new Promise (() => {
-        setTimeout(()=>{
-            arr.forEach((item)=>{
-                if(item%2 === 0){
-                    output.innerHTML += `${item} `;
-                }
-            })
-        },1000);
-        return new Promise(() =>{
-            setTimeout(()=>{
-                arr.forEach((item)=>{
-                    if(item%2 === 0){
-                        output.innerHTML += `${item*2} `;
-                    }
-                })
-            },2000);
-        })
-    })
+// Function to return a promise that resolves after 3 seconds with the array
+function createPromiseWithArray(array) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(array);
+        }, 3000);
+    });
 }
 
-ChainedPromises();
+// Chained promises to manipulate the array
+createPromiseWithArray(arr)
+    .then((array) => {
+        // Filter out odd numbers
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                const filteredArray = array.filter((num) => num % 2 === 0);
+                output.innerHTML = `Filtered (Even Numbers): ${filteredArray.join(", ")}<br>`;
+                resolve(filteredArray);
+            }, 1000);
+        });
+    })
+    .then((filteredArray) => {
+        // Multiply even numbers by 2
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                const transformedArray = filteredArray.map((num) => num * 2);
+                output.innerHTML += `Transformed (Doubled Even Numbers): ${transformedArray.join(", ")}`;
+                resolve(transformedArray);
+            }, 2000);
+        });
+    });
