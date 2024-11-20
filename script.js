1356d118943daf5d34f731586420b8222898311b
manojkalyan27 <147662541+manojkalyan27@users.noreply.github.com>
2024-11-20 16:48:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 //your JS code here. If required.
+let arr = [1, 2, 3, 4];
+
+let output = document.querySelector('#output');
+
+function ChainedPromises(){
+
+	return new Promise (() => {
+        setTimeout(()=>{
+            arr.forEach((item)=>{
+                if(item%2 === 0){
+                    output.innerHTML += `${item} `;
+                }
+            })
+        },1000);
+        return new Promise(() =>{
+            setTimeout(()=>{
+                arr.forEach((item)=>{
+                    if(item%2 === 0){
+                        output.innerHTML += `${item*2} `;
+                    }
+                })
+            },2000);
+        })
+    })
+}
+
+ChainedPromises();
